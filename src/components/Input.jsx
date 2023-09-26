c3429ad6948fe66e06cb3273b943b9c818e2448c
Praneetha29 <praneetha2829@gmail.com>
2023-09-26 10:47:09 +0530

    feat: Add Code Editor component

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,32 +1,73 @@
 import React, { useState } from 'react';
+import AceEditor from 'react-ace';
+import 'ace-builds/src-noconflict/mode-sql';
+import 'ace-builds/src-noconflict/theme-dracula';
+
+import '../styles/Input.css';
 
 function Input() {
-    const [inputValue, setInputValue] = useState('');
+  const [inputValue, setInputValue] = useState('');
+  const [codeHistory, setCodeHistory] = useState([]);
+  const [selectedCodeIndex, setSelectedCodeIndex] = useState(-1);
 
-    const handleInputChange = (e) => {
-        setInputValue(e.target.value);
-    };
+  const handleInputChange = (newValue) => {
+    setInputValue(newValue);
+  };
 
-    const handleExecuteClick = () => {
+  const handleExecuteClick = () => {
+    setCodeHistory([...codeHistory, inputValue]);
+    setInputValue('');
+  };
 
-        console.log('Executing query:', inputValue);
+  const handleCopyClick = () => {
+    navigator.clipboard.writeText(inputValue);
+  };
 
+  const handleClearClick = () => {
+    setInputValue('');
+  };
 
-        setInputValue('');
-    };
+  const handleCodeHistoryClick = (index) => {
+    setSelectedCodeIndex(index);
+    setInputValue(codeHistory[index]);
+  };
 
-    return (
-        <div className="input">
-            <h2>Input</h2>
-            <textarea
-                rows="5"
-                placeholder="Enter your SQL query here..."
-                value={inputValue}
-                onChange={handleInputChange}
-            ></textarea>
-            <button onClick={handleExecuteClick}>Execute</button>
-        </div>
-    );
+  return (
+    <div className="input">
+      <h2>Input</h2>
+      <div className="code-editor">
+        <AceEditor
+          mode="sql"
+          theme="dracula"
+          onChange={handleInputChange}
+          value={inputValue}
+          placeholder="Enter your SQL query here..."
+          fontSize={14}
+          width="100%"
+          height="200px"
+        />
+      </div>
+      <div className="input-buttons">
+        <button onClick={handleExecuteClick}>Execute</button>
+        <button onClick={handleCopyClick}>Copy</button>
+        <button onClick={handleClearClick}>Clear</button>
+      </div>
+      <div className="code-history">
+        <h3>Code History</h3>
+        <ul>
+          {codeHistory.map((code, index) => (
+            <li
+              key={index}
+              className={index === selectedCodeIndex ? 'selected' : ''}
+              onClick={() => handleCodeHistoryClick(index)}
+            >
+              {code}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default Input;
